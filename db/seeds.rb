192bd28edba44c272723f541dffc3e8cb90fdde9
Madiha Latafi <latafimadiha@hotmail.fr>
2017-11-29 14:20:15 +0100

    back-end done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "0612159091",
+    category:     "french"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number:  "0612159092",
+    category:     "chinese"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number:  "0612159093",
+    category:     "italian"
+  },
+  {
+    name:         "Timbre",
+    address:      "rue Sainte-Beuve 75006 Paris",
+    phone_number:  "0612159094",
+    category:     "japanese"
+  },
+  {
+    name:         "Braisenville",
+    address:      "rue Condorcet, 75009 Paris",
+    phone_number:  "0612159095",
+    category:     "belgian"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
